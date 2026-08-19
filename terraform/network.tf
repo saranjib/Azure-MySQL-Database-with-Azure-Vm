c59cd7f62a2b3fad792c603b287f818878a18395
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-08-19 11:15:01 +0530

    Add network security group and subnet association

---
+++ terraform/network.tf
@@ -0,0 +1,34 @@
+resource "azurerm_network_security_group" "app" {
+  name                = "nsg-app-vm"
+  location            = azurerm_resource_group.rg-azure-mysql-vm.location
+  resource_group_name = azurerm_resource_group.rg-azure-mysql-vm.name
+
+  security_rule {
+    name                       = "allow-http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "app" {
+  subnet_id                 = azurerm_subnet.app.id
+  network_security_group_id = azurerm_network_security_group.app.id
+}
